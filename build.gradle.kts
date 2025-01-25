e46c113f1a5ef8994ee8455e483fff6c685589a3
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-01-25 18:54:20 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
 }
 
 tasks.register<Delete>("clean").configure {
